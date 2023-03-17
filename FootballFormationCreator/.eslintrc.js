2568faec48f34ca66bbededb00cef8806c93f9f0
raghibdarr <raghibdarr@gmail.com>
2023-03-17 01:38:41 +0000

    fixed prettier line ending issue

--- FootballFormationCreator/.eslintrc.js
+++ FootballFormationCreator/.eslintrc.js
@@ -1,4 +1,15 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    '@react-native-community',
+    'plugin:prettier/recommended', // Add this line
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
 };
